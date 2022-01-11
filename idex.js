4f73949e2fa7a7a4501001095f538352a56d5871
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-11 10:42:19 +0100

    map

---
+++ idex.js
@@ -0,0 +1,15 @@
+const zahlen = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
+const ungeradeZahlen = [];
+const geradeZahlen = [];
+function arraysOrdnen (zahlenMap){
+      if (zahlenMap %2 ==0) {    
+        geradeZahlen.push(zahlenMap);
+        }
+        else{
+          ungeradeZahlen.push(zahlenMap);
+        }
+    }
+
+zahlen.map(arraysOrdnen);
+console.log(ungeradeZahlen);
+console.log(geradeZahlen);
