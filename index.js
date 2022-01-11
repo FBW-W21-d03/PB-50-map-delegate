5db599053885ea36b99b83f2b8b375b0e4bc6e4f
kawanaza <kawanbabani@gmail.com>
2022-01-11 10:53:14 +0100

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const myNumber = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
 const geradenZahlen = [];
 const ungeradeZahlen = [];
 myNumber.map((parametZahl) => {
-  if (parametZahl % 2==0) {
+  if (parametZahl % 2) {
     ungeradeZahlen.push(parametZahl);
   } else {
     geradenZahlen.push(parametZahl);
