cd26c1c99efaa4f9e6101b20d2689eeb27aefa79
sprotte <metatron1972@live.de>
2022-01-11 10:53:11 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 "use strict";
 
-const array = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
-const negativeArray = [];
-const positiveArray = [];
-array.map((paramForEach) => {
-  if (paramForEach < 0) {
-    negativeArray.push(paramForEach);
+const zahlen = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
+const ungeradeZahlen = [];
+const geradeZahlen = [];
+function arraysOrdnen(zahlenMap) {
+  if (zahlenMap % 2 == 0) {
+    geradeZahlen.push(zahlenMap);
   } else {
-    positiveArray.push(paramForEach);
+    ungeradeZahlen.push(zahlenMap);
   }
-});
-console.log("Alle Zahlen:", array);
-console.log("Negative numbers:", negativeArray);
-console.log("Possitive numbers:", positiveArray);
+}
+
+zahlen.map(arraysOrdnen);
+console.log(ungeradeZahlen);
+console.log(geradeZahlen);
