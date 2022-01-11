ed904699a17cd08b16206dc3e80af8b11c061705
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-11 10:53:22 +0100

    PB50

---
+++ index.js
@@ -0,0 +1,19 @@
+// Teil 1
+
+const ungeradeZahlen = [];
+const geradeZahlen = [];
+
+// Teil 2
+
+function zuweisen(zahlen) {
+    if (zahlen % 2 == 0) {
+        geradeZahlen.push(zahlen);
+    } else {
+        ungeradeZahlen.push(zahlen);
+    }
+}
+// Teil 3
+const zahlenKette = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
+zahlenKette.map(zuweisen);
+console.log("ungerade Zahlen:" , ungeradeZahlen);
+console.log("gerade Zahlen:" , geradeZahlen);
