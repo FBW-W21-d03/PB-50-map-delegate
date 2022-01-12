61ddb12b6bb20f0c92cfb025c95e07da06a645d6
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-12 14:37:11 +0100

    ANDERE LÖSUNG MIT VEWENDUNG VON FILTER

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const zahlen = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
+ const zahlen = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
 
-const geradeZahlen = [];
-const ungeradeZahlen = [];
+ const geradeZahlen = [];
+ const ungeradeZahlen = [];
 
-function ordnen(zahl) {
+ function ordnen(zahl) {
  
-  if (zahl % 2 == 0) {
-    ungeradeZahlen.push(zahl);
-  } else {
-    geradeZahlen.push(zahl);
-  }
+     if (zahl % 2 == 0) {
+         ungeradeZahlen.push(zahl);
+         } else {
+           geradeZahlen.push(zahl);
+      }
   
-}
+  }
 
-zahlen.map(ordnen);
-console.log("zahlen:", `${zahlen}`);
-console.log("geradeZahlen:", `${geradeZahlen}`);
-console.log("ungeradeZahlen:",` ${ungeradeZahlen}`);
+ zahlen.map(ordnen);
+ console.log("zahlen:", `${zahlen}`);
+ console.log("geradeZahlen:", `${geradeZahlen}`);
+ console.log("ungeradeZahlen:",`${ungeradeZahlen}`);
 
+
+
+
+ 
+
+// ANDERE LÖSUNG MIT VEWENDUNG VON FILTER:
+const numbers = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
+const evenNumbers = numbers.filter((number) => number % 2 == 0);
+const oddNumbers = numbers.filter((number) => number % 2 == 1);
+
+ console.log("evenNumbers:", `${evenNumbers}`);
+ console.log("oddNumbers:", `${oddNumbers}`);
