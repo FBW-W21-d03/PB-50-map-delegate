6f384c2252dba6b4cfa4bd6127a706af77ed2ec5
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-12 13:52:33 +0100

    map mit return

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ function zuweisen(zahlen) {
     }
 }
 
-// Teil 3
+/*
+Teil 3 - map mit return
+*/
 const zahlenKette = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
 zahlenKette.map(zuweisen);
+// inkrementieren aller Zahlen im Array
+const array2 = zahlenKette.map(
+    (zuweisen) => {
+        return zuweisen + 1;
+    }
+);
+
+
 console.log("ungerade Zahlen:" , ungeradeZahlen);
 console.log("gerade Zahlen:" , geradeZahlen);
