f02f3e2bdf97debbb4bfc31f8f215b6fb25f7e78
ShiinyFlakes <mago_elm@hotmail.com>
2022-01-12 14:01:45 +0100

    fixed

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ const zahlen = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
 
 const zahlen2 = zahlen.map(
     (item)=>{
-return item%2 ==0;
+        if(item%2 ==0){
+            return item
+        }if (item%2 ==1) {
+         return item;
+        }
     }
 );
-
-/*function myfunc(zahl){
+/*
+function myfunc(zahl){
     if(zahl%2 == 0){
-geradeZahlen.push(zahl);
+geradeZahlen.map(zahl);
     }else{
-        ungeradeZahlen.push(zahl)
+        ungeradeZahlen.map(zahl)
     }
 }
 zahlen.map(myfunc);*/
