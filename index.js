1748b2e460407898771e1e3cba3f4d22c3117451
slavablack <veaceslav.ciornii@magenta.de>
2022-01-12 14:11:45 +0100

    map return

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const numbers= [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
+numbers.sort();
+
 const geradeZahl=[];
 const ungeradeZahl=[];
 function geradeUngerade (param5){
@@ -9,7 +11,25 @@ function geradeUngerade (param5){
         
     }
 }
+
+
 numbers.map(geradeUngerade)
 console.log(numbers)
 console.log(ungeradeZahl)
 console.log(geradeZahl)
+// Neue Aufgabe
+const numbersnew= [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
+numbersnew.sort();
+
+const arrayGerade = numbersnew.map((gerade) => {
+if (gerade %2 ==0) {
+    return gerade;
+}
+});
+ const arrayUngerade = numbersnew.map((ungerade) => {
+     if (ungerade %2 ==1) {
+        return ungerade;
+    }
+   });
+console.log(arrayGerade);
+console.log(arrayUngerade);
