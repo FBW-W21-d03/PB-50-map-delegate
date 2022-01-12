7372003cca3efd24009fe837443d27e50ae0c2dd
sprotte <metatron1972@live.de>
2022-01-12 15:23:29 +0100

    sprotte

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ console.log(ungeradeZahlen);
 console.log(geradeZahlen);
 */
 
+/* RETURN*/
+/*
 const ungeradeZahlen = [];
 const geradeZahlen = [];
 
@@ -29,3 +31,11 @@ const zahlen1 = zahlen.map((item) => {
     return item;
   }
 });
+*/
+
+const nummer = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
+const ersteNummer = nummer.filter((nummer) => nummer % 2 == 0);
+const zweiteNummer = nummer.filter((nummer) => nummer % 2 == 1);
+
+console.log("grade:", `${ersteNummer}`);
+console.log("ungrade:", `${zweiteNummer}`);
