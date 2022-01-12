565d52d1ca5088933f8e0a0004612d301f253d14
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-12 14:19:06 +0100

    mein lösung

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 const geradeZahlen = [];
 const ungeradeZahlen = [];
 /*const zahlen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55];*/
+const zahlen = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
 
 
-function arraysSorting(item) {
+const zahlen2 = zahlen.map((item) => {
+  if (item % 2 == 0) {
+    geradeZahlen.push(item);
+    return geradeZahlen;
+  }else {
+    ungeradeZahlen.push(item);
+    return ungeradeZahlen;
+  }
+ 
+  
+});
+
+/*function arraysSorting(item) {
   if (item % 2 == 0) {
     geradeZahlen.push(item);
   } else {
     ungeradeZahlen.push(item);
   }
-}
+}*/
 
-const zahlen = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
 
-zahlen.map(arraysSorting);
-console.log(ungeradeZahlen);
 console.log(geradeZahlen);
+console.log(ungeradeZahlen);
+console.log(zahlen2);
+
+
+/*zahlen.map(arraysSorting);
+console.log(ungeradeZahlen);
+console.log(geradeZahlen);*/
+
+
+
+/*const array = [1, 2, 3, 4, 5, 6, 7];
+
+// inkrementieren aller Zahlen im Array
+const array2 = array.map((item) => {
+  return item + 1;
+});
+console.log(array2);*/
