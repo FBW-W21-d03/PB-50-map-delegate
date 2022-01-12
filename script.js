efbf9a59245c41d49e3690e2828397ed89ca7616
ShiinyFlakes <mago_elm@hotmail.com>
2022-01-12 13:47:16 +0100

    revised

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ const geradeZahlen = [];
 
 const zahlen = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
 
+const zahlen2 = zahlen.map(
+    (item)=>{
+return item%2 ==0;
+    }
+);
 
-function myfunc(zahl){
+/*function myfunc(zahl){
     if(zahl%2 == 0){
 geradeZahlen.push(zahl);
     }else{
         ungeradeZahlen.push(zahl)
     }
 }
-zahlen.map(myfunc);
+zahlen.map(myfunc);*/
 console.log(ungeradeZahlen);
-console.log(geradeZahlen);
+console.log(zahlen2);
+
