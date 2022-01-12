3c5a3db837da21d4915c98bde2a95741b7390cf0
Mazen <mazen.taha88@gmail.com>
2022-01-12 15:25:54 +0100

    My Lösung2

--- index.js
+++ index.js
@@ -13,3 +13,32 @@ function myFunction(zahl) {
 zahlen.map(myFunction);
 console.log(geradeZahlen);
 console.log(ungeradeZahlen)
+
+
+/*
+map mit return
+*/
+const zahlen =[9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
+
+const array2 = zahlen.map(
+    (item) => {
+        if(item % 2 == 0){
+            return item
+        }if (item % 2 == 1) {
+         return item;
+        }
+    }
+);
+
+
+/*
+map mit Filter
+*/ 
+
+const zahlen = [9, 1, 4, 7, 2, 56, 3, 33, 89, 76, 473];
+
+const geradeZahlen = zahlen.filter((zahl) => zahl % 2 == 0);
+const ungeradeZahlen = zahlen.filter((zahl) => zahl % 2 == 1);
+
+console.log(geradeZahlen);
+console.log(ungeradeZahlen);
